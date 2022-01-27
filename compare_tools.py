b24ed8690b342afbc5ec7f74f4565b164f1481aa
Alexa VanHattum <alexavanhattum@gmail.com>
2022-01-27 09:23:11 -0500

    More comments, formatting

--- compare_tools.py
+++ compare_tools.py
@@ -34,29 +34,36 @@ SUCCESS = "SUCCESS"
 UNKNOWN = "UNKNOWN"
 
 def check_verification_result(cmd, success_str, failure_strs):
-    print("RUNNING command:", " ".join(cmd))
+    """Run a command with subprocess and check whether the result contains
+    the tool-specific verification success string, or any of the possible
+    failure strings (passed as a list).
+    """
+    print("\tRunning command:", " ".join(cmd))
     result = subprocess.run(cmd, capture_output=True)
     output = str(result.stdout)
     success_found = success_str in output
     failure_found = any([f in output for f in failure_strs])
 
     if success_found and not failure_found:
-        print("\tCommand succeeded, found:", success_str)
+        print("\t\tSUCCESS, found:", success_str)
         return SUCCESS
     elif not success_found and failure_found:
-        print("\tCommand failed, found:", "/".join(failure_str))
+        print("\t\tFAILURE, found:", "/".join(failure_strs))
         return FAILURE
     elif success_found and failure_found:
-        print("\tExpected either success or failure, found both: {} {}", success_str, "/".join(failure_strs))
+        print("\t\tUNKNOWN, expected either success or failure, found both: {} {}".format(success_str, "/".join(failure_strs)))
         print(output)
         return UNKNOWN
     else:
-        print("\tExpected either success or failure, found neither: {} {}",  success_str, "/".join(failure_strs))
+        print("\t\tUNKNOWN, Expected either success or failure, found neither: {} {}".format(success_str, "/".join(failure_strs)))
         print(output)
         return UNKNOWN
 
 
 def check_kani():
+    """Check results for Kani run on the single Rust test files.
+    """
+    print("\n---------------------- Checking results for Kani ----------------------")
     for test_name in TESTS:
         test = os.path.join(TEST_DIR, KANI_DIR, test_name + ".rs")
         cmd = ["kani", test]
@@ -67,11 +74,18 @@ def check_kani():
         res = check_verification_result(cmd, "VERIFICATION SUCCESSFUL", ["VERIFICATION FAILED"])
 
 def check_crux_mir():
+    """Check results for Crux-MIR run on the single Rust test files.
+    """
+    print("\n------------------- Checking results for Crux-MIR ---------------------")
     for test_name in TESTS:
         test = os.path.join(TEST_DIR, CRUX_MIR_DIR, test_name + ".rs")
         res = check_verification_result(["/root/.cabal/bin/crux-mir", test], "Overall status: Valid.", ["Overall status: Invalid."])
 
 def check_rvt_klee():
+    """Check results for Rust Verification Tools - KLEE, which needs to be run 
+    on crates where each case is a propverify test.
+    """
+    print("\n------------------ Checking results for RVT - KLEE --------------------")
     for test_name in TESTS:
         test = os.path.join(TEST_DIR, RVT_DIR, test_name, "Cargo.toml")
         res = check_verification_result(["cargo-verify", "--backend=klee", "--tests", "--manifest-path", test], "VERIFICATION_RESULT: VERIFIED", ["VERIFICATION_RESULT: ERROR", "VERIFICATION_RESULT: UNKNOWN"])
@@ -79,9 +93,13 @@ def check_rvt_klee():
 
 
 def check_rvt_seahorn():
+    """Check results for Rust Verification Tools - Seahorn, which needs to be run 
+    on crates where each case is a propverify test.
+    """
+    print("\n---------------- Checking results for RVT - Seahorn -------------------")
     for test_name in TESTS:
         test = os.path.join(TEST_DIR, RVT_DIR, test_name, "Cargo.toml")
-        res = check_verification_result(["cargo-verify", "--backend=seahorn", "--tests", "--manifest-path", test], "VERIFICATION_RESULT: VERIFIED", "VERIFICATION_RESULT: ERROR")
+        res = check_verification_result(["cargo-verify", "--backend=seahorn", "--tests", "--manifest-path", test], "VERIFICATION_RESULT: VERIFIED", ["VERIFICATION_RESULT: ERROR", "VERIFICATION_RESULT: UNKNOWN"])
         result = subprocess.run(["cargo", "clean", "--manifest-path", test], capture_output=True)
 
 def main():
